4748610d615cbfd872b08132e5bd13fc1aa20c74
timas <vas.liwanov@yandex.ru>
2021-02-04 19:46:56 +0300

    fix

--- public/scripts/accordion.js
+++ public/scripts/accordion.js
@@ -36,6 +36,7 @@
         $target.addClass('managers__person-header_open');
         $parent.find('.managers__details').slideDown('200');
         $parent.addClass('managers__person_active');
+        $(window).scrollTop($target.offset().top);
       }, 300);
     });
   };
